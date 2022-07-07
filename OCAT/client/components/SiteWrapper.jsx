0b9c13ad340c67a749d5362041999cdb9e49f490
yogilany <yogilany@gmail.com>
2022-07-07 19:40:47 -0400

    changed style

--- OCAT/client/components/SiteWrapper.jsx
+++ OCAT/client/components/SiteWrapper.jsx
@@ -5,7 +5,7 @@ import { Navigation } from '.';
 export const SiteWrapper = ({ children }) => <>
   <Navigation />
 
-  <main role="main" className="flex-shrink-0" style={{ paddingTop: `5rem` }}>
+  <main role="main" className="flex-shrink-0" style={{ paddingTop: `4rem` }}>
     <Container>
       <Row>
         <Col>
