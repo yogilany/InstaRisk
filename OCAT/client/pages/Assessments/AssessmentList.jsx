136538b90c1d89ed8d246e51c13a56d2aeb58a77
yogilany <yogilany@gmail.com>
2022-07-13 22:01:36 -0400

    implement react-table on component

--- OCAT/client/pages/Assessments/AssessmentList.jsx
+++ OCAT/client/pages/Assessments/AssessmentList.jsx
@@ -1,8 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import { useTable } from 'react-table';
 import { AssessmentService } from '../../services/AssessmentService';
 
 export const AssessmentList = () => {
   const [ assessments, setAssessments ] = useState([]);
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: `ID`,
+        accessor: `id`, // accessor is the "key" in the data
+      },
+      {
+        Header: `Cat Name`,
+        accessor: `cat_name`,
+      },
+      {
+        Header: `Cat Date of Birth`,
+        accessor: `cat_date_of_birth`,
+      },
+      {
+        Header: `Risk Level`,
+        accessor: `risk_level`,
+      },
+      {
+        Header: `Score`,
+        accessor: `score`,
+      },
+      {
+        Header: `Created at`,
+        accessor: `created_at`,
+      },
+    ],
+    [],
+  );
 
   // fetch all assessments using the AssessmentService.getList function from OCAT/client/services/AssessmentService.js
   useEffect(() => {
@@ -12,12 +43,49 @@ export const AssessmentList = () => {
     fetchAssessments();
   }, []);
 
+  const {
+    getTableBodyProps,
+    getTableProps,
+    headerGroups,
+    prepareRow,
+    rows,
+  } = useTable(
+    {
+      columns,
+      data: assessments,
+    },
+  );
+
   return (
     <div>
-      {/*
-          List goes here
-          Please use the library react-table https://www.npmjs.com/package/react-table
-      */}
+      <table {...getTableProps()} style={{ border: `solid 1px blue` }}>
+        <thead>
+          {headerGroups.map(headerGroup =>
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column =>
+                <th
+                  {...column.getHeaderProps()}
+                >
+                  {column.render(`Header`)}
+                </th>)}
+            </tr>)}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell =>
+                  <td
+                    {...cell.getCellProps()}
+                  >
+                    {cell.render(`Cell`)}
+                  </td>)}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
