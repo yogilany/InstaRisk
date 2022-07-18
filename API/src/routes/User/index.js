8c1c1a617b2bc5be8e95d05917ed30d555ac738a
yogilany <yogilany@gmail.com>
2022-07-18 12:53:32 -0400

    Receiving data before sending a request

--- API/src/routes/User/index.js
+++ API/src/routes/User/index.js
@@ -4,5 +4,27 @@ const { UserService } = require(`../../microservices`);
 const BASE_URL = `/user`;
 
 module.exports = server => {
+  server.post(
+    `${BASE_URL}/login`,
+    async (req, res, next) => {
+      try {
+        const { LoginData: { loginData } } = req.body;
 
+        // verify that your data is making it here to the API by using console.log(assessment);
+        // call the AssessmentService.submit function from the API/src/microservices/Assessment/ and
+        // supply the correct parameters
+        const result = await UserService.confirmCredentials(loginData);
+        // generate token
+
+        ResponseHandler(
+          res,
+          `Submitted data`,
+          { result },
+          next,
+        );
+      } catch (err) {
+        next(err);
+      }
+    },
+  );
 };
