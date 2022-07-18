296201293489d6446d2b56c6231b7a5c09f02a7e
yogilany <yogilany@gmail.com>
2022-07-18 12:47:42 -0400

    Sending request to API

--- OCAT/server/libs/UserService/index.js
+++ OCAT/server/libs/UserService/index.js
@@ -1,1 +1,22 @@
 const { client, config } = require(`../../utils`);
+const { InternalServerError } = require(`restify-errors`);
+
+exports.login = (LoginData) => new Promise((resolve, reject) => {
+
+  // this function sends a request to the API
+  // finish the logic to handle the response when returned from the API
+  client.post(`/user/login`, { LoginData },
+    (err, req, res, body) => {
+      if (err) {
+
+        return reject(err);
+      }
+
+      if (res.statusCode !== 200) {
+
+        return reject(new InternalServerError(`Request Error`));
+      }
+
+      resolve(body.data);
+    });
+});
